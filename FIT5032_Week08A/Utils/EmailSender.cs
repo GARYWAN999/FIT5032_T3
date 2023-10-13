7e9169b66728e55f9774238bf4e7c22fab688fcb
jiahao wan <jwan0177@student.monash.edu>
2023-10-13 20:06:32 +1100

    Add project files.

---
+++ FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,47 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents, HttpPostedFileBase postedFile)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("502824312@qq.com", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            if (postedFile != null)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    // 1. convert attach file to inputStream
+                    postedFile.InputStream.CopyTo(memoryStream);
+                    // 2. convert inputstream to byte array
+                    byte[] bytes = memoryStream.ToArray();
+                    Attachment attachment = new Attachment();
+                    // 3. convert to base64 type(String)
+                    attachment.Content = Convert.ToBase64String(bytes);
+                    attachment.Filename = postedFile.FileName;
+                    // 4. add this attachment to email message
+                    msg.AddAttachment(attachment.Filename, attachment.Content);
+                }
+            }
+
+
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
